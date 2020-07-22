3ad43a965bfca920b2041994ae40a7fa6c244302
Abdelilah-Majid <abdodijam15@gmail.com>
2020-07-22 17:44:10 +0100

    adding some additional unfenished symbol decoding with a hint on next step

--- javascript/decoding-symbols.js
+++ javascript/decoding-symbols.js
@@ -53,5 +53,71 @@ function decode(char){
 		return true;
 	}else if(char==="?"){
 		return true;
+	}else{
+		return false;
 	}
 }
+
+function trakeNextSymbol(firstSymbol,secendSymbol){
+	if(firstSymbol!==secendSymbol){
+		return false;
+	}else{
+		if(firstSymbol==="="&&secendSymbol==="="){
+			return "=";
+		}else if(firstSymbol==="=="&&secendSymbol==="="){
+			return"=";
+		}else if(firstSymbol==="!"&&secendSymbol==="="){
+			return"=";
+		}else if(firstSymbol==="!="&&secendSymbol==="="){
+			return"=";
+		}else if(firstSymbol==="&"&&secendSymbol==="&"){
+			return"&";
+		}else if(firstSymbol==="|"&&secendSymbol==="|"){
+			return"|";
+		}else if(firstSymbol==="<"&&secendSymbol==="="){
+			return"=";
+		}else if(firstSymbol===">"&&secendSymbol==="="){
+			return"=";
+		}else{
+			return false;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d.decode=decode;
+d.tNS=trakeNextSymbol;
--- javascript/main.js
+++ javascript/main.js
@@ -8,7 +8,12 @@ let $in_rj=$("#input-ryujin");
 let in_val="";
 let in_val_checkout="";
 let in_array_index=0;
+let in_array_chartAt_index=0;
 let in_array=[];
+let first_type=false;
+let iACAI_IAI_relation={};
+
+
 
 
 
@@ -19,19 +24,40 @@ $out_js.on("focus",function(){
 
 // console.log($in_rj);
 $in_rj.on('keyup',function(e){
+	console.log(in_array_chartAt_index);
+	if(first_type===false){
+		first_type=true;
+		this.value="";
+	}
 	in_val=this.value;
 	console.log(in_val);
 	if(in_val_checkout!==in_val){
-		for (var i = 0; i < in_val.length; i++) {
-			if(in_array[in_array_index]===undefined){
-				in_array[in_array_index]="";
-			}
-
-
-
+		if(in_array[in_array_index]===undefined){
+			in_array[in_array_index]="";
 		}
+		// console.log(e);
+		if(in_val_checkout.length<in_val.length){
+			//NOTE: use iACAI_IAI_relation for your next step;
+			if(d.decode(e.key)){
+				console.log(d.tNS(in_array[in_array_index].charAt(in_array_chartAt_index),e.key));
+				if(d.tNS(in_array[in_array_index].charAt(in_array_chartAt_index),e.key)){
+					console.log("it work");
+					in_array[in_array_index]+=e.key;
+					in_array_chartAt_index++;
+				}else{
+					in_array_index++;
+					in_array[in_array_index]="";
+					in_array[in_array_index]+=e.key;
+					in_array_chartAt_index++;
 
+				}
+			}else{
+				in_array[in_array_index]+=e.key;
+				in_array_chartAt_index++;
+			}
+		}
 		console.log(in_array);
 		in_val_checkout=in_val;
 	}
-})
+
+});
