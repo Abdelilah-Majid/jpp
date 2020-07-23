67d78b7be9e5a6f9547a407ed16ef41a922c9e66
Abdelilah-Majid <abdodijam15@gmail.com>
2020-07-23 21:58:46 +0100

    fixsing some bugs and adding a non completed in_rj traker

--- javascript/decoding-symbols.js
+++ javascript/decoding-symbols.js
@@ -59,12 +59,12 @@ function decode(char){
 }
 
 function trakeNextSymbol(firstSymbol,secendSymbol){
-	if(firstSymbol!==secendSymbol){
-		return false;
-	}else{
-		if(firstSymbol==="="&&secendSymbol==="="){
+	if(true){
+		if(firstSymbol==="=="&&secendSymbol==="="){
+			console.log("its ==");
 			return "=";
-		}else if(firstSymbol==="=="&&secendSymbol==="="){
+		}else if(firstSymbol==="="&&secendSymbol==="="){
+			console.log('its =');
 			return"=";
 		}else if(firstSymbol==="!"&&secendSymbol==="="){
 			return"=";
@@ -79,6 +79,7 @@ function trakeNextSymbol(firstSymbol,secendSymbol){
 		}else if(firstSymbol===">"&&secendSymbol==="="){
 			return"=";
 		}else{
+			console.log("tNS: key does not match");
 			return false;
 		}
 	}
--- javascript/main.js
+++ javascript/main.js
@@ -11,7 +11,7 @@ let in_array_index=0;
 let in_array_chartAt_index=0;
 let in_array=[];
 let first_type=false;
-let iACAI_IAI_relation={};
+let iACAI_IAI_relation={0:-1};
 
 
 
@@ -19,45 +19,60 @@ let iACAI_IAI_relation={};
 
 // console.log($out_js);
 $out_js.on("focus",function(){
-	$(this).blur();
+	$(this).blur();//if you focus on in_js it blur it out
 });
 
 // console.log($in_rj);
 $in_rj.on('keyup',function(e){
-	console.log(in_array_chartAt_index);
 	if(first_type===false){
-		first_type=true;
 		this.value="";
+		first_type=true;//clear last sessen code
 	}
 	in_val=this.value;
-	console.log(in_val);
+	// console.log(in_val);
 	if(in_val_checkout!==in_val){
 		if(in_array[in_array_index]===undefined){
-			in_array[in_array_index]="";
+			in_array[in_array_index]="";//if in_array current var === undefined mak it emty string
 		}
+
 		// console.log(e);
-		if(in_val_checkout.length<in_val.length){
+		if(in_val_checkout.length<in_val.length){//if the user is adding
 			//NOTE: use iACAI_IAI_relation for your next step;
-			if(d.decode(e.key)){
-				console.log(d.tNS(in_array[in_array_index].charAt(in_array_chartAt_index),e.key));
-				if(d.tNS(in_array[in_array_index].charAt(in_array_chartAt_index),e.key)){
-					console.log("it work");
-					in_array[in_array_index]+=e.key;
-					in_array_chartAt_index++;
+			if(d.decode(e.key)){//if input is a symbol
+				// console.log(d.tNS(in_array[in_array_index].charAt(iACAI_IAI_relation[in_array_index]),e.key));
+				if(d.tNS(in_array[in_array_index],e.key)){//if here is a compelet sharacter
+					console.log("it auto");
+					// console.log("it work");
+					in_array[in_array_index]+=d.tNS(in_array[in_array_index].charAt(iACAI_IAI_relation[in_array_index]),e.key);//add input
+					in_array_chartAt_index++;//rise the character tracer by 1;
+					iACAI_IAI_relation[in_array_index]=in_array_chartAt_index-1;
 				}else{
-					in_array_index++;
-					in_array[in_array_index]="";
-					in_array[in_array_index]+=e.key;
-					in_array_chartAt_index++;
+					console.log("its defult");
+					in_array_index++;//put the current key into the next array var
+					in_array_chartAt_index=0;
+					in_array[in_array_index]="";//clear undefined
+					in_array[in_array_index]+=e.key;//add key
+					in_array_chartAt_index++;//rise the character tracer by 1;
+					iACAI_IAI_relation[in_array_index]=in_array_chartAt_index-1;
 
 				}
 			}else{
-				in_array[in_array_index]+=e.key;
-				in_array_chartAt_index++;
+				in_array[in_array_index]+=e.key;//add key
+				in_array_chartAt_index++;//rise the character tracer by 1;
+				iACAI_IAI_relation[in_array_index]=in_array_chartAt_index-1;
 			}
 		}
+		// console.log(in_array_chartAt_index);
+		// console.log(in_array_index);
+		// if("="){console.log("= is working");}
+		// if("=="){console.log("== is working");}
+		console.log("in array.chartAt:",in_array[in_array_index].charAt(iACAI_IAI_relation[in_array_index]));
+		console.log("in array:",in_array[in_array_index]);
+		console.log("key",e.key);
+		console.log("d.tNS",d.tNS(in_array[in_array_index].charAt(iACAI_IAI_relation[in_array_index]),e.key));
+		console.log(iACAI_IAI_relation);
 		console.log(in_array);
-		in_val_checkout=in_val;
+		in_val_checkout=in_val;//end if();
 	}
 
 });
