dda479411f851e6dc3f29f00f3a37562bc9c8789
Abdelilah-Majid <abdodijam15@gmail.com>
2020-08-13 15:44:43 +0100

    adding the secend examle

--- javascript/examples.js
+++ javascript/examples.js
@@ -25,6 +25,7 @@ $img3=$('<img src="./example-1-imgs/3.jpg" alt="" id="div-img3">');
 
 // con.log($img0.children());
 
+$div_1.children().remove();
 $div_1.app($img0,$img1,$img2,$img3);
 $div_1.children("img").css("width","25vw");
 $div_1.children("img").css("height","35vh");
@@ -207,6 +208,42 @@ $img0.when('mouseleave',func(e){
 
 
 
+let example_1=`sps accordion_content="this is the accordion content ------- Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+sps $accordion=$(\`<ul class='accordion'>
+		<h1>toggel accordion to see the effect</h1>
+		<li>
+			<button class='accordion-controler' type='button' name='button'>toggel accordion 0</button>
+			<div class='accordion-panel'>
+
+			</div>
+		</li>
+		<li>
+			<button class='accordion-controler' type='button' name='button'>toggel accordion 1</button>
+			<div class='accordion-panel'>
+
+			</div>
+		</li>
+		<li>
+			<button class='accordion-controler' type='button' name='button'>toggel accordion 2</button>
+			<div class='accordion-panel'>
+
+			</div>
+		</li>
+	</ul>\`);
+$(".div").children().delete();
+$(".div").app($accordion);
+$(".div").children("ul").children("h1").css({color:"yellowgreen"})
+$(".accordion-controler").css({width:"100%"})
+$(".accordion-panel").text(accordion_content);
+$('.accordion').when("click",'.accordion-controler',func(e){//when you use ()=>{this and $(this) returns window} but when you use func(){this returns what it shold return}
+e.preventDefault();
+$(this)
+	.next(".accordion-panel")
+	.no(":animated")
+	.sT();
+
+});`
 
 
 
@@ -227,8 +264,71 @@ $img0.when('mouseleave',func(e){
 
 
 
+let example_2=`
+var request,$current;
+var cache={};
+var $frame=$("#photo-viewer");
+var $thumbs=$(".thumb");
 
+function crossfade($img){
 
+if ($current){
+	$current.stop().fadeOut("slow");
+}
+
+$img.css({
+	marginLeft:-$img.width()/2,
+	marginTop:-$img.height()/2
+});
+
+$img.stop().fadeTo("slow",1);
+
+$current=$img;
+}
+
+$(document).on("click",".thumb",function(e){
+var $img;
+var src=this.href;
+request=src;
+
+e.preventDefault();
+
+$thumbs.removeClass("active");
+$(this).addClass("active");
+
+if (cache.hasOwnProperty(src)) {
+	if (cache[src].isLoading===false){
+		crossfade(cache[src].$img);
+	}
+}else{
+	$img=$("<img/>");
+	cache[src]={
+		$img:$img,
+		isLoading:true
+	};
+	$img.on("load",function(){
+		$img.hide();
+
+		$frame.removeClass("is-loading").append($img);
+		cache[src].isLoading=false;
+
+		if (request===src) {
+			crossfade($img);
+		}
+	})
+
+	$frame.addClass("is-loading");
+	console.log(this);
+	$img.attr({
+		"src":src,
+		"alt":this.title||""
+	})
+}
+
+});
+
+$(".thumb").eq(0).click();
+`
 
 
 
@@ -328,5 +428,8 @@ $img0.when('mouseleave',func(e){
 
 
 $buttons_array[0].on('click',function(e){
-	$in_rj.val(example_0);
+	$in_rj_1.val(example_0);
 });
+$buttons_array[1].on("click",function(e){
+	$in_rj_1.val(example_1);
+})
